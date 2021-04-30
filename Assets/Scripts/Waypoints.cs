47bb246387d4ec86aa33ce3d4af4373f020d8191
BGokgoz <barangokgoz99@gmail.com>
2021-05-01 02:29:05 +0300

    Patrol & Detection

---
+++ Assets/Scripts/Waypoints.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    
+    [SerializeField]
+    protected float debugDrawRadius = 1.0F;
+    public virtual void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, debugDrawRadius);
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
