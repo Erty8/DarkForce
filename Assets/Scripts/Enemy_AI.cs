1d32ebd61c6262d38ed54447dd3c1a2168128439
BGokgoz <barangokgoz99@gmail.com>
2021-05-05 00:47:31 +0300

    Build v0.01
    https://farcell.itch.io/darkforce

--- Assets/Scripts/Enemy_AI.cs
+++ Assets/Scripts/Enemy_AI.cs
@@ -195,7 +195,7 @@ public class Enemy_AI : MonoBehaviour
             anim.SetBool("continueAttack", true);
         }
         //yield return new WaitUntil(() => attackCooldown == true);
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(3f);
         anim.SetBool("attack", false);
         yield return new WaitUntil(() => attackIndex == 3);
         anim.SetBool("continueAttack", false);
