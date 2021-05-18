012bf304cb5459231a299610b378612418f4fc0d
Erty8 <41906495+Erty8@users.noreply.github.com>
2021-05-18 16:09:02 +0300

    Trap

---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    public float trapDamage = 35;
+    public float detectRange = 20;
+    public float timeBeforeInvisible = 2f;
+    public List<GameObject> enemies = new List<GameObject>();
+    GameObject closestEnemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(invisible());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FindClosestEnemy(); 
+        if (Vector3.Distance(closestEnemy.transform.position, transform.position) <= detectRange)
+        {
+            gameObject.GetComponent<MeshRenderer>().enabled = true;
+            StartCoroutine(invisible());
+        }
+    }
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            col.GetComponent<PlayerCombat>().takeDamage(trapDamage);
+            Destroy(gameObject);
+        }
+        if (col.tag == "Enemy")
+        {
+            col.GetComponent<EnemyCombatScript>().takeDamage(trapDamage);
+            col.GetComponent<Animator>().SetBool("takeHit", true);
+            Destroy(gameObject);
+        }
+    }
+    void FindClosestEnemy()
+    {
+        List<GameObject> enemies = new List<GameObject>();
+        enemies.AddRange(GameObject.FindGameObjectsWithTag("Player"));
+        float distanceToClosestEnemy = Mathf.Infinity;
+
+        foreach (GameObject currentEnemy in enemies)
+        {
+            float distanceToEnemy = (currentEnemy.transform.position - this.transform.position).sqrMagnitude;
+            if (distanceToEnemy < distanceToClosestEnemy)
+            {
+                distanceToClosestEnemy = distanceToEnemy;
+                closestEnemy = currentEnemy;
+            }
+        }
+        //transform.position = Vector3.MoveTowards(transform.position, closestEnemy.transform.position, step);
+        
+    }
+    IEnumerator invisible()
+    {
+        gameObject.GetComponent<MeshRenderer>().enabled = true;
+        yield return new WaitForSeconds(timeBeforeInvisible);
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+
+    }
+}
